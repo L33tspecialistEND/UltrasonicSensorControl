3bc076b2eee53366290b0d02de92b1a37ad067f2
Ntiaju Chukwuebuka Eric <ntiajubukason@gmail.com>
2025-05-30 11:45:27 +0100

    Add a loop to read distance every 2 seconds

--- ultrasonicSensor.cpp
+++ ultrasonicSensor.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <thread>
 #include <modbus/modbus.h>          // library to communicate with the sensor
 
 int main() {
@@ -14,32 +15,39 @@ int main() {
         return 1;
     }
     
-    for (int iii{}; iii < numSlaves; ++iii)
+    // Iterate 5 times to read from the 2 sensors every 2 seconds
+    for (int jjj{}, times{5}; jjj <= times; ++jjj)
     {
-        if (modbus_set_slave(ctx, iii + 1) == -1)
+        // Iterate twice to read from 2 sensors
+        for (int iii{}; iii < numSlaves; ++iii)
         {
-            std::cerr << "Invalid slave ID\n";
-            modbus_free(ctx);
-            return 1;
-        }
+            if (modbus_set_slave(ctx, iii + 1) == -1)
+            {
+                std::cerr << "Invalid slave ID\n";
+                modbus_free(ctx);
+                return 1;
+            }
 
-        if (modbus_connect(ctx) == -1)
-        {
-            std::cerr << "Connection failed\n";
-            modbus_free(ctx);
-            return 1;
-        }
+            if (modbus_connect(ctx) == -1)
+            {
+                std::cerr << "Connection failed\n";
+                modbus_free(ctx);
+                return 1;
+            }
 
-        uint16_t distance{};
-        int rc = modbus_read_registers(ctx, 0x0100, 1, &distance);
-        if (rc == -1)
-        {
-            std::cerr << "Failed to read register\n";
-        } 
-        else 
-        {
-            std::cout << "Sensor #" << iii + 1 << " responded. Distance: " << distance << " mm" << std::endl;
+            uint16_t distance{};
+            int rc = modbus_read_registers(ctx, 0x0100, 1, &distance);
+            if (rc == -1)
+            {
+                std::cerr << "Failed to read register\n";
+            } 
+            else 
+            {
+                std::cout << "Sensor #" << iii + 1 << " responded. Distance: " << distance << " mm" << std::endl;
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
         }
+        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
     }
 
     modbus_close(ctx);
