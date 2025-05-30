501fedaed0703bc49fb7062997fb14b9dbd916d1
Ntiaju Chukwuebuka Eric <ntiajubukason@gmail.com>
2025-05-30 11:36:01 +0100

    Add a loop to read distance from 2 sensors

--- ultrasonicSensor.cpp
+++ ultrasonicSensor.cpp
@@ -3,7 +3,7 @@
 
 int main() {
     const char* port{ "/dev/ttyUSB0" };     // USB port name
-    int slave_id{ 1 };                      // default address for all sensors
+    int numSlaves{ 2 };                     // Number of sensors to communicate with
 
     // Creates a pointer to a structure with defaults for sensor communication
     modbus_t* ctx = modbus_new_rtu(port, 9600, 'N', 8, 1);
@@ -14,24 +14,32 @@ int main() {
         return 1;
     }
     
-    if (modbus_set_slave(ctx, slave_id) == -1) {
-        std::cerr << "Invalid slave ID\n";
-        modbus_free(ctx);
-        return 1;
-    }
+    for (int iii{}; iii < numSlaves; ++iii)
+    {
+        if (modbus_set_slave(ctx, iii + 1) == -1)
+        {
+            std::cerr << "Invalid slave ID\n";
+            modbus_free(ctx);
+            return 1;
+        }
 
-    if (modbus_connect(ctx) == -1) {
-        std::cerr << "Connection failed\n";
-        modbus_free(ctx);
-        return 1;
-    }
+        if (modbus_connect(ctx) == -1)
+        {
+            std::cerr << "Connection failed\n";
+            modbus_free(ctx);
+            return 1;
+        }
 
-    uint16_t distance{};
-    int rc = modbus_read_registers(ctx, 0x0100, 1, &distance);
-    if (rc == -1) {
-        std::cerr << "Failed to read register\n";
-    } else {
-        std::cout << "Sensor responded. Distance: " << distance << " mm" << std::endl;
+        uint16_t distance{};
+        int rc = modbus_read_registers(ctx, 0x0100, 1, &distance);
+        if (rc == -1)
+        {
+            std::cerr << "Failed to read register\n";
+        } 
+        else 
+        {
+            std::cout << "Sensor #" << iii + 1 << " responded. Distance: " << distance << " mm" << std::endl;
+        }
     }
 
     modbus_close(ctx);
